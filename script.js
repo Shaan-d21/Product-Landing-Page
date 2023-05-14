392a43b6f567194b309db45bddf8e11be5e24220
Shaan Dewang <shaan042004@gmail.com>
2023-05-15 00:24:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let menue = document.querySelector('#menue-icon')
+let navmenue = document.querySelector('.nav-options')
+
+menue.addEventListener('click',()=>{
+    navmenue.classList.toggle('change')
+})
